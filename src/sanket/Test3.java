2c70caf292e29dd1f970d4955a65c2153c351975
Sankemate <sanketmate2911@gmail.com>
2024-12-15 20:45:36 +0530

    This is to sending to rannamate using branch

---
+++ src/sanket/Test3.java
@@ -0,0 +1,11 @@
+package sanket;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello sanket This is Test 3 are you fine");
+
+	}
+
+}
